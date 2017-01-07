914b7e9134f9c56ab2bc90f78bae9fcc32e72b38
Kai Guo <kai.guo@vervemobile.com>
2017-01-06 16:33:21 -0800

    try ngreact library with angular

--- ui/gulpfile.babel.js
+++ ui/gulpfile.babel.js
@@ -11,7 +11,6 @@ import size       from 'gulp-size';
 import csso       from 'gulp-csso';
 import filter     from 'gulp-filter';
 import concatCss  from 'gulp-concat-css';
-import babel      from 'gulp-babel';
 
 import fs  from 'fs';
 import del from 'del';
@@ -49,9 +48,6 @@ const PATH = {
   js: [
     './src/**/*.js',
   ],
-  jsx: [
-    './src/**/*.jsx',
-  ],
   vendor: [
     './node_modules/moment/moment.js',
     './node_modules/angular/angular.js',
@@ -67,10 +63,8 @@ const PATH = {
     './node_modules/angular-loading-bar/build/loading-bar.js',
     './node_modules/bootstrap-slider/js/*.js',
 
-    // './node_modules/react/dist/react.js',
-    // './node_modules/react-dom/dist/react-dom.js',
-    './node_modules/inferno/dist/inferno.js',
-    './node_modules/inferno/dist/inferno-compat.js',
+    './node_modules/react/dist/react.js',
+    './node_modules/react-dom/dist/react-dom.js',
     './node_modules/ngreact/ngReact.js',
 
     '!**/*.min.js',
@@ -138,25 +132,9 @@ gulp.task('copy.assets', function() {
 });
 
 gulp.task('copy.js', function() {
-  return gulp.src(PATH.js)
-      .pipe(babel(
-        {
-          "presets": ["es2015", "stage-1"],
-          "plugins": [
-              ["module-resolver", {
-                  // "root": ["."],
-                  "alias": {
-                      "react": "inferno-compat",
-                      "react-dom": "inferno-compat"
-                  }
-              }],
-              "transform-react-jsx",
-              'transform-runtime'
-          ]
-        }
-      ))
-      .pipe(concat('app.js'))
-      .pipe(gulp.dest(PATH.build));
+  return performChain('js', PATH.js, PATH.build+"/", (chain) => {
+    return chain.pipe(concat('app.js'));
+  });
 });
 
 gulp.task('copy.vendor', function() {
@@ -243,8 +221,6 @@ gulp.task('minify', function() {
       .pipe(size())
       .pipe(gulp.dest(PATH.dist));
 });
-
-
 
 // Combos -------------------------------------------------------------
 
